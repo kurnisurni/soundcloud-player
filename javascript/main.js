9a544d668a4f3d441bd837bc3722417af9bf3a18
Kurnia <kurniasuryani@gmail.com>
2020-12-28 15:30:02 +0100

    dynamic music cards

--- javascript/main.js
+++ javascript/main.js
@@ -21,57 +21,62 @@ SoundcloudAPI.getTrack = function(inputValue) {
         q: inputValue
     }).then(function(tracks) {
         console.log(tracks);
+        SoundcloudAPI.renderTracks(tracks);
     });
 }
 
-SoundcloudAPI.getTrack("Sepultura");
+SoundcloudAPI.getTrack("Towa Tei");
 
-SoundcloudAPI.renderTracks = function() {
+SoundcloudAPI.renderTracks = function(tracks) {
 
-    //card
-    var card = document.createElement('div');
-    card.classList.add('card');
+    tracks.forEach(function(track) {
+        //card
+        var card = document.createElement('div');
+        card.classList.add('card');
 
-    //image
-    var imageDiv = document.createElement('div');
-    imageDiv.classList.add('image');
+        //image
+        var imageDiv = document.createElement('div');
+        imageDiv.classList.add('image');
 
-    var image_img = document.createElement('img');
-    image_img.classList.add('image_img');
-    image_img.src = "http://www.placekitten.com/290/290";
+        var image_img = document.createElement('img');
+        image_img.classList.add('image_img');
+        image_img.src = track.artwork_url || 'http://lorempixel.com/100/100/abstract';
 
-    imageDiv.appendChild(image_img);
+        imageDiv.appendChild(image_img);
 
-    //content
-    var content = document.createElement('div');
-    content.classList.add('content');
+        //content
+        var content = document.createElement('div');
+        content.classList.add('content');
 
-    var header = document.createElement('div');
-    header.classList.add('header');
-    header.innerHTML = '<a href="#" target="_blank">Science vs. Romance</a>';
+        var header = document.createElement('div');
+        header.classList.add('header');
+        header.innerHTML = '<a href="' + track.permalink_url + '" target="_blank">' + track.title + '</a>';
 
-    //button
-    var button = document.createElement('div');
-    button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+        //button
+        var button = document.createElement('div');
+        button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
 
-    var icon = document.createElement('i');
-    icon.classList.add('add', 'icon');
+        var icon = document.createElement('i');
+        icon.classList.add('add', 'icon');
 
-    var buttonText = document.createElement('span');
-    buttonText.innerHTML = 'Add to playlist';
+        var buttonText = document.createElement('span');
+        buttonText.innerHTML = 'Add to playlist';
 
-    //append child
-    content.appendChild(header);
+        //append child
+        content.appendChild(header);
 
-    button.appendChild(icon);
-    button.appendChild(buttonText);
+        button.appendChild(icon);
+        button.appendChild(buttonText);
 
-    card.appendChild(imageDiv);
-    card.appendChild(content);
-    card.appendChild(button);
+        card.appendChild(imageDiv);
+        card.appendChild(content);
+        card.appendChild(button);
 
-    var searchResults = document.querySelector(".js-search-results");
-    searchResults.appendChild(card);
+        var searchResults = document.querySelector(".js-search-results");
+        searchResults.appendChild(card);
+    });
+
+
 }
 
 SoundcloudAPI.renderTracks();
