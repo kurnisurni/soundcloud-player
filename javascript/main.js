2de5b04a2ce427cdf99fb8e9c746c142e324702e
Kurnia <kurniasuryani@gmail.com>
2020-12-28 15:11:41 +0100

    adding new card

--- javascript/main.js
+++ javascript/main.js
@@ -27,8 +27,49 @@ SoundcloudAPI.getTrack = function(inputValue) {
 SoundcloudAPI.getTrack("Sepultura");
 
 SoundcloudAPI.renderTracks = function() {
+
+    //card
     var card = document.createElement('div');
-    card.classList.add("card");
+    card.classList.add('card');
+
+    //image
+    var imageDiv = document.createElement('div');
+    imageDiv.classList.add('image');
+
+    var image_img = document.createElement('img');
+    image_img.classList.add('image_img');
+    image_img.src = "http://www.placekitten.com/290/290";
+
+    imageDiv.appendChild(image_img);
+
+    //content
+    var content = document.createElement('div');
+    content.classList.add('content');
+
+    var header = document.createElement('div');
+    header.classList.add('header');
+    header.innerHTML = '<a href="#" target="_blank">Science vs. Romance</a>';
+
+    //button
+    var button = document.createElement('div');
+    button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+    var icon = document.createElement('i');
+    icon.classList.add('add', 'icon');
+
+    var buttonText = document.createElement('span');
+    buttonText.innerHTML = 'Add to playlist';
+
+    //append child
+    content.appendChild(header);
+
+    button.appendChild(icon);
+    button.appendChild(buttonText);
+
+    card.appendChild(imageDiv);
+    card.appendChild(content);
+    card.appendChild(button);
+
     var searchResults = document.querySelector(".js-search-results");
     searchResults.appendChild(card);
 }
